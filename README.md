eee37c67e3642a2d75c0d68f181e289ddf7bd715
chenjun <1547678430@qq.com>
2025-10-22 14:57:32 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # OpenWRT-CI
+
 云编译OpenWRT固件
 
 官方版：
@@ -8,8 +9,6 @@ https://github.com/immortalwrt/immortalwrt.git
 https://github.com/VIKINGYFY/immortalwrt.git
 
 # 固件简要说明：
-
-固件每天早上4点自动编译。
 
 固件信息里的时间为编译开始的时间，方便核对上游源码提交时间。
 
@@ -23,5 +22,4 @@ Scripts——自定义脚本
 
 Config——自定义配置
 
-#
-[![Stargazers over time](https://starchart.cc/VIKINGYFY/OpenWRT-CI.svg?variant=adaptive)](https://starchart.cc/VIKINGYFY/OpenWRT-CI)
+
---
+++ Scripts/Feeds.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+# echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.default
+# echo 'src-git modem https://github.com/FUjr/modem_feeds.git' >> feeds.conf.default
+echo 'src-git easytier https://github.com/EasyTier/luci-app-easytier.git' >>feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >>feeds.conf.default
