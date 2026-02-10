1b5b41e2543c56595ea65790a125d25c5e5874a4
“Alienven” <alienven@outlook.com>
2026-02-10 18:19:59 +0800

    update

--- Scripts/Feeds.sh
+++ Scripts/Feeds.sh
@@ -8,6 +8,6 @@
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 # echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.default
 # echo 'src-git modem https://github.com/FUjr/modem_feeds.git' >> feeds.conf.default
+# echo 'src-git luci_app_xray https://github.com/yichya/luci-app-xray.git' >>feeds.conf.default
 echo 'src-git easytier https://github.com/EasyTier/luci-app-easytier.git' >>feeds.conf.default
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >>feeds.conf.default
-echo 'src-git luci_app_xray https://github.com/yichya/luci-app-xray.git' >>feeds.conf.default
